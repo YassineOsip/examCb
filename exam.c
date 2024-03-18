9004afcfb74ff2d428a15b91648f49fbed3ad46b
yassineosip <yassine.lf.99@gmail.com>
2024-03-18 04:26:10 +0000

    first commit

---
+++ exam.c
@@ -0,0 +1,30 @@
+#include "exam.h"
+
+// LEVEL 1
+int add(int a, int b)
+{
+    return 0;
+}
+
+// LEVEL 2
+int find_max(int num1, int num2)
+{
+    return 0;
+}
+
+// LEVEL 3
+char *reverse_string(char *str)
+{
+    return "";
+}
+
+// LEVEL 4
+int is_prime(int number)
+{
+    return 0;
+}
+
+// LEVEL 5
+int is_palindrome(char *str) {
+    return 0;
+}
---
+++ exam.h
@@ -0,0 +1,14 @@
+#ifndef EXAM_H
+#define EXAM_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int add(int a, int b); // LEVEL 1
+int find_max(int num1, int num2); // LEVEL 2
+char *reverse_string(char *str); // LEVEL 3
+int is_prime(int number); // LEVEL 4
+int is_palindrome(char *str); // LEVEL 5
+
+#endif
